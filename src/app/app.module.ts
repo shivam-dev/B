fb07204a5c4ec994031de3e0220579f0cab7bc72
Shivam Gupta <shivam.gupta6@harman.com>
2017-08-29 16:59:11 +0530

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,55 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { OrganicTrackerSheetComponent } from './organic-tracker-sheet/organic-tracker-sheet.component';
+import { CommonService } from './common/common.service';
+import { FormsModule }    from '@angular/forms';
+
+import {
+  MaterialModule,
+  MdAutocompleteModule,
+  MdButtonModule,
+  MdButtonToggleModule,
+  MdCardModule,
+  MdCheckboxModule,
+  MdChipsModule,
+  MdCoreModule,
+  MdDatepickerModule,
+  MdDialogModule,
+  MdExpansionModule,
+  MdGridListModule,
+  MdIconModule,
+  MdInputModule,
+  MdListModule,
+  MdMenuModule,
+  MdNativeDateModule,
+  MdPaginatorModule,
+  MdProgressBarModule,
+  MdProgressSpinnerModule,
+  MdRadioModule,
+  MdRippleModule,
+  MdSelectModule,
+  MdSidenavModule,
+  MdSliderModule,
+  MdSlideToggleModule,
+  MdSnackBarModule,
+  MdSortModule,
+  MdTableModule,
+  MdTabsModule,
+  MdToolbarModule,
+  MdTooltipModule
+} from '@angular/material';
+import { MainComponent } from './main/main.component';
+
 
 const appRoutes: Routes = [
-  { path: 'app-login', component: LoginComponent },
-  { path: 'app-organic-tracker-sheet',      component: OrganicTrackerSheetComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'organic-tracker-sheet', component: OrganicTrackerSheetComponent },
+  { path: 'main', component: MainComponent },
   { path: '',
     redirectTo: '/login',
     pathMatch: 'full'
@@ -17,19 +58,56 @@ const appRoutes: Routes = [
 
 
 @NgModule({
+
   declarations: [
     AppComponent,
     LoginComponent,
-    OrganicTrackerSheetComponent
+    OrganicTrackerSheetComponent,
+    MainComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
+    FormsModule,
     RouterModule.forRoot(
       appRoutes,
       { enableTracing: true } // <-- debugging purposes only
-    )
+    ),
+
+    MaterialModule,
+    MdAutocompleteModule,
+    MdButtonModule,
+    MdButtonToggleModule,
+    MdCardModule,
+    MdCheckboxModule,
+    MdChipsModule,
+    MdCoreModule,
+    MdDatepickerModule,
+    MdDialogModule,
+    MdExpansionModule,
+    MdGridListModule,
+    MdIconModule,
+    MdInputModule,
+    MdListModule,
+    MdMenuModule,
+    MdNativeDateModule,
+    MdPaginatorModule,
+    MdProgressBarModule,
+    MdProgressSpinnerModule,
+    MdRadioModule,
+    MdRippleModule,
+    MdSelectModule,
+    MdSidenavModule,
+    MdSliderModule,
+    MdSlideToggleModule,
+    MdSnackBarModule,
+    MdSortModule,
+    MdTableModule,
+    MdTabsModule,
+    MdToolbarModule,
+    MdTooltipModule,
   ],
-  providers: [],
+  providers: [CommonService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+import { CommonService } from '../common/common.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
 
-  constructor() { }
+export class LoginComponent  {
 
-  ngOnInit() {
+   router:any;
+   username:any;
+   password: any;
+   commonService:any;
+
+   constructor(_router: Router, _commonService: CommonService){   
+       this.router = _router;
+       this.commonService = _commonService;
+   }
+	
+
+  getLogin(values){
+    this.commonService.setUsername(values.username);
+    this.router.navigateByUrl('/main');
   }
 
 }
